c805e26eb7c3240070287d044dd21043eab845c9
lqw <cslgdxlqw@126.com>
2016-07-19 10:02:32 +0800

    增加了FileInfo

--- src/com/lqw666/hello/App.java
+++ src/com/lqw666/hello/App.java
@@ -1,5 +1,14 @@
 package com.lqw666.hello;
 
 public class App {
+	public static void main(String[] args) {
+		sayHello();
+	}
+	private static void sayHello() {
+		
+		System.out.println("hello");
+
+	}
+	
 
 }
---
+++ src/com/lqw666/hello/FileInfo.java
@@ -0,0 +1,20 @@
+package com.lqw666.hello;
+
+public class FileInfo {
+	private String name;
+
+	public FileInfo(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
